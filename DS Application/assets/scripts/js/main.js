9c1207561cf02eed05bdc02f7f6d6b8d25439bef
Becca <Becca37@puckrat.com>
2025-12-12 15:57:17 -0700

    .

--- DS Application/assets/scripts/js/main.js
+++ DS Application/assets/scripts/js/main.js
@@ -250,7 +250,7 @@ window.addEventListener("load", myInit, true); function myInit(){
         }
     );
     loadCsvIntoTable(
-        "output/csv/crosstab/Crosstab_Complexity_by_Rating",
+        "output/csv/crosstab/Crosstab_Complexity_by_Rating.csv",
         "complexity-by-rating-table",
         {
             decimals: 1,
